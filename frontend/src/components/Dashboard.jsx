f60a6757f844f7c9938eeea1a875008f0d8ff173
mcamaya <camiamayar@gmail.com>
2023-10-13 12:50:00 -0500

    feat: :sparkles: Modal funciona

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,18 +1,47 @@
-import React, { useEffect } from 'react';
-import ProductCard from './ProductCard';
-import './css/Dashboard.css'
+import React, { useEffect, useState, lazy, Suspense } from 'react';
+import './css/Dashboard.css';
+const ProductCard = lazy(() => import('./ProductCard'));
 
 export default function Dashboard() {
-  useEffect(() => {
+  const urlApi = 'http://localhost:5010/producto';
+  const [productos, setProductos] = useState([]);
 
+  useEffect(() => {
+    fetch(urlApi, {
+      method: "GET",
+      headers: {
+        "Accept-version": "1.0.0",
+      },
+    })
+      .then((res) => res.json())
+      .then(({data}) => setProductos(data))
+      .catch((err) => console.log(err));
   }, []);
 
+  console.log(productos);
+
+  if (!productos) return (
+    <h2>Loading...</h2>
+  )
   
   return (
     <div className='dashboard'>
       <h1>Dashboard</h1>
       <div className="products-container">
-        <ProductCard />
+        {
+          productos.map(({_id, nombre, imagen, descripcion, precio}, index) => (
+            <Suspense>
+              <ProductCard 
+              key={index}
+              nombre={nombre}
+              imagen={imagen}
+              precio={precio}
+              descripcion={descripcion}
+              id={_id}
+            />
+            </Suspense>
+          ))
+        }
       </div>
     </div>
   )
